fed690d093dd73cc84339eda34cf96f5fd8d045f
AnveshThada <anvesh1265@gmail.com>
2018-08-11 21:59:07 +0530

    Print statement added

--- Calculator/src/source/Calculator.java
+++ Calculator/src/source/Calculator.java
@@ -5,6 +5,7 @@ public class Calculator {
 	public int add(int i, int j) {
 		System.out.println("I - "+i);
 		System.out.println("J - "+j);
+		System.out.println("Console Printed");
 		return (i+j);
 	}
 }
